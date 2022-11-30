45c6824a9c2d9141c0746ee6abbee1d1bf514a6f
Denis Loban <xxx@zzz.yyy>
2022-12-01 00:16:13 +0300

    test gcb

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 //-----------------------------------------------------------------------------
 // NodeJS App for GCP Cloud Functions deployed via GCP Cloud Build Triggers
-// Copyleft (c)
 //-----------------------------------------------------------------------------
 
 exports.helloWorld = (req, res) => {
-  const message="<font color='blue'>СloudFunction of Denis Loban!</font><br><b>App Version 1.1</b>";
+  const message="<font color='blue'>СloudFunction of staging!</font><br><b>App Version 1.1</b>";
   res.status(200).send(message);
 };
